99a4ef26e827c1b596efc70f288ccb17183f1bb5
ahmed-saleh98 <ahmed.mohamed.saleh1998@gmail.com>
2023-06-19 18:54:59 +0300

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import Head from 'next/head';
+import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
@@ -9,6 +10,24 @@ export default function App({ Component, pageProps }) {
         <title>Ahmed Saleh</title>
         <meta name="description" content="Ahmed Saleh's Personal Portfolio" />
       </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-FJ4Y1Z3VQ6"
+      ></Script>
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FJ4Y1Z3VQ6', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </>
